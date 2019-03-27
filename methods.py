c299f393f3698d881215832dcfbf11a057e71fa1
DylanCa <dylan.cattelan@gmail.com>
2019-03-27 10:05:13 +0100

    Finished Caesar Cipher Encode / Decode (need to optimize the code though)

--- methods.py
+++ methods.py
@@ -24,9 +24,9 @@ class Methods:
                     decoded_text.append([])
                     for char in self.encrypted_text[current_index]:
                         letter = 122 - (
-                            shift_to_apply - 1 - ord(char) -
-                            97) if ord(char) - shift_to_apply < 97 else ord(
-                                char) - shift_to_apply
+                            shift_to_apply - 1 - (ord(char) - 97)
+                        ) if ord(char) - shift_to_apply < 97 else ord(
+                            char) - shift_to_apply
 
                         decoded_text[current_index] += chr(letter)
 
@@ -34,7 +34,7 @@ class Methods:
                         decoded_text[current_index])
                     current_index += 1
 
-                print("Here's your encoded text with a Shift of {}: {}".format(
+                print("Here's your decoded text with a Shift of {}: {}".format(
                     shift_to_apply, ' '.join(decoded_text)))
 
         else:
@@ -45,24 +45,19 @@ class Methods:
             while current_index < len(self.encrypted_text):
                 decoded_text.append([])
                 for char in self.encrypted_text[current_index]:
-                    if ord(char) + shift_to_apply < 97:
-                        letter = 122 - (shift_to_apply - ord(char) - 97)
+                    letter = 122 - (
+                        shift_to_apply - 1 - (ord(char) - 97)
+                    ) if ord(char) - shift_to_apply < 97 else ord(
+                        char) - shift_to_apply
 
-                    elif ord(char) + shift_to_apply > 122:
-                        letter = 97 + (shift_to_apply + ord(char) - 122)
-
-                    else:
-                        letter = ord(char) + shift_to_apply
-
-                    if letter > 96 and letter < 123:
-                        decoded_text[current_index] += chr(letter)
+                    decoded_text[current_index] += chr(letter)
 
                 decoded_text[current_index] = ''.join(
                     decoded_text[current_index])
                 current_index += 1
 
-            print('{}: {}'.format("Here's your encoded text",
-                                  ' '.join(decoded_text)))
+            print("Here's your decoded text with a Shift of {}: {}".format(
+                shift_to_apply, ' '.join(decoded_text)))
 
     def encodeCaesarCipher(self):
         while (True):
